d0c6c90bf859eaa5b149136a6e86eef910273e5a
shoonya <83222785+shadowbhai10@users.noreply.github.com>
2021-04-27 07:20:34 +0000

    Update redditBot.py

--- redditBot.py
+++ redditBot.py
@@ -1,17 +1,15 @@
-
 try:
-  import praw // First you have to install this library.
-  subreddit_name = raw_input("Enter the subreddit name ")
+  import praw 	#install this library to create bots in reddit
+  subreddit_name = str(input("Enter the subreddit name "))
   r = praw.Reddit('bot1')
   subreddit = r.subreddit(subreddit_name)
-expect Exception as e:
+  for read in subreddit.hot(limit=5): #you can choose any function .new() or .top()
+    print("Title: ",read.title)         #This will show title;
+    print("Text: ",read.selftext)       #This will show the text; 
+    print("Score: ",read.score)         #This will show current score or upvotes;
+    print("--------------------------------------------------------------------------------\n")      
+
+except Exception as e:
   print(e)
 
-for read in subreddit.hot(limit=5):   //You can choose .new() or .top() function also;
-  print("Title: ",read.title)         //This will show title;
-  print("Text: ",read.selftext)       //This will show the text; 
-  print("Score: ",read.score)         //This will show current score or upvotes; 
-  print("--------------------------------------------------------------------------------\n)
-                                      //Since the output will be top 5 so we need a seperator;
-        
         
